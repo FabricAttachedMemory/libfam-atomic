13c6a294903160c8ffc15a0740eb006bb1e72c96
Jason Low <jason.low2@hp.com>
2015-08-28 14:16:12 -0700

    Various library updates
    Signed-off-by: Jason Low <jason.low2@hp.com>

--- fam_atomic.c
+++ fam_atomic.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <assert.h>
+#include <signal.h>
 #include "fam_atomic.h"
 
 #define LOCK_PREFIX_HERE                  \
@@ -86,9 +87,9 @@ static __thread struct list *fam_atomic_region_list = NULL;
 /* Each node represents a registered NVM atomic region. */
 struct node {
 	void *region_start;
-	int64_t length;
+	size_t region_length;
 	int fd;
-	int64_t offset;
+	off_t offset;
 	/* Next node in the linked list. */
 	struct node *next;
 };
@@ -97,13 +98,13 @@ struct list {
 	struct node *head;
 };
 
-void list_add(struct list *list, struct node *node)
+static void list_add(struct list *list, struct node *node)
 {
 	node->next = list->head;
 	list->head = node;
 }
 
-void list_del(struct list *list, struct node *prev, struct node *node)
+static void list_del(struct list *list, struct node *prev, struct node *node)
 {
 	if (!prev) {
 		assert(list->head == node);
@@ -115,7 +116,7 @@ void list_del(struct list *list, struct node *prev, struct node *node)
 	free(node);
 }
 
-int fam_atomic_register(void *address, int64_t length, int fd, int64_t offset)
+int fam_atomic_register_region(void *region_start, size_t region_length, int fd, off_t offset)
 {
 	struct node *new_node = malloc(sizeof(new_node));
 
@@ -131,8 +132,8 @@ int fam_atomic_register(void *address, int64_t length, int fd, int64_t offset)
 		fam_atomic_region_list->head = NULL;
 	}
 
-	new_node->region_start = address;
-	new_node->length = length;
+	new_node->region_start = region_start;
+	new_node->region_length = region_length;
 	new_node->fd = fd;
 	new_node->offset = offset;
 
@@ -141,15 +142,15 @@ int fam_atomic_register(void *address, int64_t length, int fd, int64_t offset)
 	return 0;
 }
 
-void fam_atomic_unregister_region(void *address, int64_t length)
+void fam_atomic_unregister_region(void *region_start, size_t region_length)
 {
 	struct node *curr, *prev;
 
 	prev = NULL;
 
 	for (curr = fam_atomic_region_list->head; curr != NULL; curr = curr->next) {
-		if (curr->region_start == address &&
-		    curr->length == length)
+		if (curr->region_start == region_start &&
+		    curr->region_length == region_length)
 			break;
 
 		prev = curr;
@@ -159,7 +160,7 @@ void fam_atomic_unregister_region(void *address, int64_t length)
 	if (!curr)
 		return;
 
-	list_delete(fam_atomic_region_list, prev, curr);
+	list_del(fam_atomic_region_list, prev, curr);
 }
 
 /*
@@ -177,7 +178,7 @@ static void fam_atomic_get_fd_offset(void *address, int *fd, int64_t *offset)
 
 	for (curr = fam_atomic_region_list->head; curr != NULL; curr = curr->next) {
 		if (curr->region_start <= address &&
-		    curr->region_start + curr->length >= address)
+		    curr->region_start + curr->region_length >= address)
 			break;
 	}
 
@@ -185,8 +186,11 @@ static void fam_atomic_get_fd_offset(void *address, int *fd, int64_t *offset)
 	 * No region containing the atomic has been registered.
 	 */
 	if (!curr) {
+		/*
+		 * Generate a segmentation fault.
+		 */
 		printf("Error: NVM region not registered as fam-atomic region?");
-		exit(1);
+		kill(0, SIGSEGV);
 	}
 
 	*fd = curr->fd;
@@ -219,6 +223,12 @@ int64_t fam_atomic_64_read_unpadded(int64_t *address)
 	return value;
 }
 
+extern void fam_atomic_128_read_unpadded(int64_t *address, int64_t result[2])
+{
+	/* TODO: Not supported on this version of the library */
+	fam_atomic_128_not_supported_in_this_version();
+}
+
 void fam_atomic_32_write_unpadded(int32_t *address, int32_t value)
 {
 	int fd;
@@ -238,6 +248,13 @@ void fam_atomic_64_write_unpadded(int64_t *address, int64_t value)
 	/* Only a write operation, no need to return prev value. */
 	x86_xchg(address, value);
 }
+
+void fam_atomic_128_write_unpadded(int64_t *address, int64_t value[2])
+{
+	/* TODO: Not supported on this version of the library */
+	fam_atomic_128_not_supported_in_this_version();
+}
+
 
 int32_t fam_atomic_32_swap_unpadded(int32_t *address, int32_t value)
 {
@@ -265,8 +282,14 @@ int64_t fam_atomic_64_swap_unpadded(int64_t *address, int64_t value)
 	return prev;
 }
 
+void fam_atomic_128_swap_unpadded(int64_t *address, int64_t value[2], int64_t result[2])
+{
+	/* TODO: Not supported on this version of the library */
+	fam_atomic_128_not_supported_in_this_version();
 
-int fam_atomic_32_compare_and_store_unpadded(int32_t *address,
+}
+
+int32_t fam_atomic_32_compare_and_store_unpadded(int32_t *address,
 				int32_t expected, int32_t desired)
 {
 	int32_t prev;
@@ -277,10 +300,10 @@ int fam_atomic_32_compare_and_store_unpadded(int32_t *address,
 	
 	prev = x86_cmpxchg(address, expected, desired);
 
-	return prev == expected;
+	return prev;
 }
 
-int fam_atomic_64_compare_and_store_unpadded(int64_t *address,
+int64_t fam_atomic_64_compare_and_store_unpadded(int64_t *address,
 					  int64_t expected, int64_t desired)
 {
 	int64_t prev;
@@ -291,7 +314,15 @@ int fam_atomic_64_compare_and_store_unpadded(int64_t *address,
 	
 	prev = x86_cmpxchg(address, expected, desired);
 
-	return prev == expected;
+	return prev;
+}
+
+void fam_atomic_128_compare_and_store_unpadded(int64_t *address,
+					       int64_t compare[2],
+					       int64_t result[2])
+{
+	/* TODO: Not supported on this version of the library */
+	fam_atomic_128_not_supported_in_this_version();
 }
 
 int32_t fam_atomic_32_fetch_and_add_unpadded(int32_t *address, int32_t increment)
@@ -322,54 +353,78 @@ int64_t fam_atomic_64_fetch_and_add_unpadded(int64_t *address, int64_t increment
 
 int32_t fam_atomic_32_read(struct fam_atomic_32 *address)
 {
-	return fam_atomic_32_read_unpadded(&address->atomic_value);
+	return fam_atomic_32_read_unpadded(&address->__v__);
 }
 
 int64_t fam_atomic_64_read(struct fam_atomic_64 *address)
 {
-	return fam_atomic_64_read_unpadded(&address->atomic_value);
+	return fam_atomic_64_read_unpadded(&address->__v__);
+}
+
+void fam_atomic_128_read(struct fam_atomic_128 *address, int64_t result[2])
+{
+	fam_atomic_128_read_unpadded(address->__v__, result);
 }
 
 void fam_atomic_32_write(struct fam_atomic_32 *address, int32_t value)
 {
-	fam_atomic_32_write_unpadded(&address->atomic_value, value);
+	fam_atomic_32_write_unpadded(&address->__v__, value);
 }
 
 void fam_atomic_64_write(struct fam_atomic_64 *address, int64_t value)
 {
-	fam_atomic_64_write_unpadded(&address->atomic_value, value);
+	fam_atomic_64_write_unpadded(&address->__v__, value);
+}
+
+void fam_atomic_128_write(struct fam_atomic_128 *address, int64_t value[2])
+{
+
+	fam_atomic_128_write_unpadded(address->__v__, value);
 }
 
 int32_t fam_atomic_32_swap(struct fam_atomic_32 *address, int32_t value)
 {
-	return fam_atomic_32_swap_unpadded(&address->atomic_value, value);
+	return fam_atomic_32_swap_unpadded(&address->__v__, value);
 }
 
 int64_t fam_atomic_64_swap(struct fam_atomic_64 *address, int64_t value)
 {
-	return fam_atomic_64_swap_unpadded(&address->atomic_value, value);
+	return fam_atomic_64_swap_unpadded(&address->__v__, value);
 }
 
-int fam_atomic_32_compare_and_store(struct fam_atomic_32 *address,
+void fam_atomic_128_swap(struct fam_atomic_128 *address, int64_t value[2], int64_t result[2])
+{
+	fam_atomic_128_swap_unpadded(address->__v__, value, result);
+}
+
+int32_t fam_atomic_32_compare_and_store(struct fam_atomic_32 *address,
 				    int32_t expected, int32_t desired)
 {
-	return fam_atomic_32_compare_and_store_unpadded(&address->atomic_value,
+	return fam_atomic_32_compare_and_store_unpadded(&address->__v__,
 							 expected, desired);
 }
 
-int fam_atomic_64_compare_and_store(struct fam_atomic_64 *address,
-				    int64_t expected, int64_t desired)
+int64_t fam_atomic_64_compare_and_store(struct fam_atomic_64 *address,
+					int64_t expected, int64_t desired)
 {
-	return fam_atomic_64_compare_and_store_unpadded(&address->atomic_value,
+	return fam_atomic_64_compare_and_store_unpadded(&address->__v__,
 							 expected, desired);
 }
+
+void fam_atomic_128_compare_and_store(struct fam_atomic_128 *address,
+				      int64_t expected[2],
+				      int64_t desired[2])
+{
+	return fam_atomic_128_compare_and_store_unpadded(address->__v__, expected, desired);
+}
+
 
 int32_t fam_atomic_32_fetch_and_add(struct fam_atomic_32 *address, int32_t increment)
 {
-	return fam_atomic_32_fetch_and_add_unpadded(&address->atomic_value, increment);
+	return fam_atomic_32_fetch_and_add_unpadded(&address->__v__, increment);
 }
 
 int64_t fam_atomic_64_fetch_and_add(struct fam_atomic_64 *address, int64_t increment)
 {
-	return fam_atomic_64_fetch_and_add_unpadded(&address->atomic_value, increment);
+	return fam_atomic_64_fetch_and_add_unpadded(&address->__v__, increment);
 }
--- fam_atomic.h
+++ fam_atomic.h
@@ -14,38 +14,56 @@
  * ----------------------
  * struct fam_atomic_32 - 32 bit fam-atomic type
  * struct fam_atomic_64 - 64 bit fam-atomic type
+ * struct fam_atomic_128 - 128 bit fam-atomic type
  */
 struct fam_atomic_32 {
-	int32_t atomic_value;
-} __attribute__((__aligned__(64)));
+	int32_t __v__ __attribute__((__aligned__(64)));
+};
 
 struct fam_atomic_64 {
-	int64_t atomic_value;
-} __attribute__((__aligned__(64)));
+	int64_t __v__ __attribute__((__aligned__(64)));
+};
+
+struct fam_atomic_128 {
+	int64_t __v__[2] __attribute__((__aligned__(64)));
+};
 
 /*
- * fam_atomic_register() - Registers an NVM region which
+ * fam_atomic_register_region() - Registers an NVM region which
  * contains fam-atomics. Must register an NVM region as an
  * fam-atomic region before any fam-atomic within the region
  * can be used.
  *
- * @address: Address of the start of the NVM region.
- * @length: The length of the NVM region.
+ * @region_start: Address of the start of the NVM region.
+ * @region_length: The length of the NVM region.
  * @fd: The file descriptor associated with the NVM region.
  * @offset: The offset from the start of the file.
  *	    (If address, is the start of the file, then offset can be 0.
  */
-extern int fam_atomic_register(void *address, int64_t length, int fd, int64_t offset);
+extern int
+fam_atomic_register_region(void *region_start,
+		    size_t region_length,
+		    int fd,
+		    off_t offset);
 
-extern void fam_atomic_unregister(void *address, int64_t length);
+extern void
+fam_atomic_unregister_region(void *region_start,
+			     size_t region_length);
 
 /*
  * Returns the value of the atomic variable.
  *
  * @address: Pointer to an fam-atomic variable.
  */
-extern int32_t fam_atomic_32_read(struct fam_atomic_32 *address);
-extern int64_t fam_atomic_64_read(struct fam_atomic_64 *address);
+extern int32_t
+fam_atomic_32_read(struct fam_atomic_32 *address);
+
+extern int64_t
+fam_atomic_64_read(struct fam_atomic_64 *address);
+
+extern void
+fam_atomic_128_read(struct fam_atomic_128 *address,
+		    int64_t result[2]);
 
 /*
  * Writes "value" to the atomic variable.
@@ -53,8 +71,17 @@ extern int64_t fam_atomic_64_read(struct fam_atomic_64 *address);
  * @address: Pointer to an fam-atomic variable.
  * @value: The value that will be written to the atomic.
  */
-extern void fam_atomic_32_write(struct fam_atomic_32 *address, int32_t value);
-extern void fam_atomic_64_write(struct fam_atomic_64 *address, int64_t value);
+extern void
+fam_atomic_32_write(struct fam_atomic_32 *address,
+		    int32_t value);
+
+extern void
+fam_atomic_64_write(struct fam_atomic_64 *address,
+		    int64_t value);
+
+extern void
+fam_atomic_128_write(struct fam_atomic_128 *address,
+		     int64_t value[2]);
 
 /*
  * Atomically writes "value" to the atomic variable and returns
@@ -63,8 +90,18 @@ extern void fam_atomic_64_write(struct fam_atomic_64 *address, int64_t value);
  * @address: Pointer to an fam-atomic variable.
  * @value: The new value that will be written to the atomic.
  */
-extern int32_t fam_atomic_32_swap(struct fam_atomic_32 *address, int32_t value);
-extern int64_t fam_atomic_64_swap(struct fam_atomic_64 *address, int64_t value);
+extern int32_t
+fam_atomic_32_swap(struct fam_atomic_32 *address,
+		   int32_t value);
+
+extern int64_t
+fam_atomic_64_swap(struct fam_atomic_64 *address,
+		   int64_t value);
+
+extern void
+fam_atomic_128_swap(struct fam_atomic_128 *address,
+		    int64_t value[2],
+		    int64_t result[2]);
 
 /*
  * Atomically checks if the atomic variable is equal to "expected"
@@ -75,10 +112,20 @@ extern int64_t fam_atomic_64_swap(struct fam_atomic_64 *address, int64_t value);
  * @expected: The value which the atomic is expected to equal.
  * @desired: The value the atomic will be set to if equal to "expected".
  */
-extern int fam_atomic_32_compare_and_store(struct fam_atomic_32 *address,
-					  int32_t expected, int32_t desired);
-extern int fam_atomic_64_compare_and_store(struct fam_atomic_64 *address,
-					  int64_t expected, int64_t desired);
+extern int32_t
+fam_atomic_32_compare_and_store(struct fam_atomic_32 *address,
+				int32_t expected,
+				int32_t desired);
+
+extern int64_t
+fam_atomic_64_compare_and_store(struct fam_atomic_64 *address,
+				int64_t expected,
+				int64_t desired);
+
+extern void
+fam_atomic_128_compare_and_store(struct fam_atomic_128 *address,
+				 int64_t expected[2],
+				 int64_t desired[2]);
 
 /*
  * Atomically adds "increment" to the atomic variable and returns the
@@ -87,10 +134,13 @@ extern int fam_atomic_64_compare_and_store(struct fam_atomic_64 *address,
  * @address: Pointer to an fam-atomic variable.
  * @increment: The value which will be added to the atomic.
  */
-extern int32_t fam_atomic_32_fetch_and_add(struct fam_atomic_32 *address,
-					   int32_t increment);
-extern int64_t fam_atomic_64_fetch_and_add(struct fam_atomic_64 *address,
-					   int64_t increment);
+extern int32_t
+fam_atomic_32_fetch_and_add(struct fam_atomic_32 *address,
+			    int32_t increment);
+
+extern int64_t
+fam_atomic_64_fetch_and_add(struct fam_atomic_64 *address,
+			    int64_t increment);
 
 /*
  * The following are the unpadded variant of the fam-atomics. Users of
@@ -98,27 +148,61 @@ extern int64_t fam_atomic_64_fetch_and_add(struct fam_atomic_64 *address,
  * own cachelines so that they do not share cachelines with regular data.
  * This is required to ensure the correctness of the atomic data.
  */
-extern int32_t fam_atomic_32_read_unpadded(int32_t *address);
-extern int64_t fam_atomic_64_read_unpadded(int64_t *address);
-extern void fam_atomic_32_write_unpadded(int32_t *address, int32_t value);
-extern void fam_atomic_64_write_unpadded(int64_t *address, int64_t value);
-extern int32_t fam_atomic_32_swap_unpadded(int32_t *address, int32_t value);
-extern int64_t fam_atomic_64_swap_unpadded(int64_t *address, int64_t value);
-extern int fam_atomic_32_compare_and_store_unpadded(int32_t *address,
-					  int32_t expected, int32_t desired);
-extern int fam_atomic_64_compare_and_store_unpadded(int64_t *address,
-					  int64_t expected, int64_t desired);
-extern int32_t fam_atomic_32_fetch_and_add_unpadded(int32_t *address,
-						     int32_t increment);
-extern int64_t fam_atomic_64_fetch_and_add_unpadded(int64_t *address,
-						     int64_t increment);
+extern int32_t
+fam_atomic_32_read_unpadded(int32_t *address);
 
-/*
- * Single node full memory barrier on x86.
- */
-static inline void memory_barrier(void)
-{
-	asm volatile("mfence" : : : "memory");
-}
+extern int64_t
+fam_atomic_64_read_unpadded(int64_t *address);
 
+extern void
+fam_atomic_128_read_unpadded(int64_t *address,
+			     int64_t result[2]);
+
+extern void
+fam_atomic_32_write_unpadded(int32_t *address,
+			     int32_t value);
+
+extern void
+fam_atomic_64_write_unpadded(int64_t *address,
+			     int64_t value);
+
+extern void
+fam_atomic_128_write_unpadded(int64_t *address,
+			      int64_t value[2]);
+
+extern int32_t
+fam_atomic_32_swap_unpadded(int32_t *address,
+			    int32_t value);
+
+extern int64_t
+fam_atomic_64_swap_unpadded(int64_t *address,
+			    int64_t value);
+
+extern void
+fam_atomic_128_swap_unpadded(int64_t *address,
+			     int64_t value[2],
+			     int64_t result[2]);
+
+extern int32_t
+fam_atomic_32_compare_and_store_unpadded(int32_t *address,
+					 int32_t expected,
+					 int32_t desired);
+
+extern int64_t
+fam_atomic_64_compare_and_store_unpadded(int64_t *address,
+					 int64_t expected,
+					 int64_t desired);
+
+extern void
+fam_atomic_128_compare_and_store_unpadded(int64_t *address,
+					  int64_t compare[2],
+					  int64_t result[2]);
+
+extern int32_t
+fam_atomic_32_fetch_and_add_unpadded(int32_t *address,
+				     int32_t increment);
+
+extern int64_t
+fam_atomic_64_fetch_and_add_unpadded(int64_t *address,
+				     int64_t increment);
 #endif
