dcbb6cb09a896ee99807ed4601dd700fb33a5761
Jason Low <jason.low2@hp.com>
2015-09-10 10:02:59 -0700

    Move definitions of locks and regular atomics to fam_atomic.h
    Signed-off-by: Jason Low <jason.low2@hp.com>

--- fam_atomic.h
+++ fam_atomic.h
@@ -1,23 +1,62 @@
 #ifndef _FAM_ATOMIC_H_
 #define _FAM_ATOMIC_H_
 
-/*
- * fam-atomic and fam-spinlock APIs. These can be used for
- * synchronization across all SOCs on the system and between
- * different cache coherence domains. fam-atomic and fam-spinlock
- * variables should not share cachelines with non fam-atomic or
- * non fam-spinlock data, and NVM regions containing these variables
- * must be registered before they can be used.
- */
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/ioctl.h>
 #include <sys/types.h>
 
+/* Fabric Attached Memory Atomic ioctl interface */
+
+struct fam_atomic_args_32 {
+        int64_t offset;
+        int32_t p32_0, p32_1;
+};
+
+struct fam_atomic_args_64 {
+        int64_t offset;
+        int64_t p64_0, p64_1;
+};
+
+struct fam_atomic_args_128 {
+        int64_t offset;
+        int64_t p128_0[2], p128_1[2];
+};
+
+#define _FAM_ATOMIC_MAGIC                       0xaa
+#define _FAM_ATOMIC_32_FETCH_AND_ADD_NR         0x11
+#define _FAM_ATOMIC_64_FETCH_AND_ADD_NR         0x12
+#define _FAM_ATOMIC_32_SWAP_NR                  0x21
+#define _FAM_ATOMIC_64_SWAP_NR                  0x22
+#define _FAM_ATOMIC_128_SWAP_NR                 0x23
+#define _FAM_ATOMIC_32_COMPARE_AND_STORE_NR     0x31
+#define _FAM_ATOMIC_64_COMPARE_AND_STORE_NR     0x32
+#define _FAM_ATOMIC_128_COMPARE_AND_STORE_NR    0x33
+#define _FAM_ATOMIC_128_READ_NR                 0x43
+#define _FAM_ATOMIC_WR(nr,type)                 _IOWR(_FAM_ATOMIC_MAGIC, (nr), type)
+#define _FAM_ATOMIC_R(nr,type)                  _IOR(_FAM_ATOMIC_MAGIC, (nr), type)
+
+/* temp = *offset; *offset += p0; p0 = temp */
+#define FAM_ATOMIC_32_FETCH_AND_ADD             _FAM_ATOMIC_WR(_FAM_ATOMIC_32_FETCH_AND_ADD_NR, struct fam_atomic_args_32)
+#define FAM_ATOMIC_64_FETCH_AND_ADD             _FAM_ATOMIC_WR(_FAM_ATOMIC_64_FETCH_AND_ADD_NR, struct fam_atomic_args_64)
+
+/* temp = *offset; *offset = p0; p0 = temp; */
+#define FAM_ATOMIC_32_SWAP                      _FAM_ATOMIC_WR(_FAM_ATOMIC_32_SWAP_NR, struct fam_atomic_args_32)
+#define FAM_ATOMIC_64_SWAP                      _FAM_ATOMIC_WR(_FAM_ATOMIC_64_SWAP_NR, struct fam_atomic_args_64)
+#define FAM_ATOMIC_128_SWAP                     _FAM_ATOMIC_WR(_FAM_ATOMIC_128_SWAP_NR, struct fam_atomic_args_128)
+
+/* temp = *offset; if (temp == p0) *offset = p1; p0 = temp; */
+#define FAM_ATOMIC_32_COMPARE_AND_STORE         _FAM_ATOMIC_WR(_FAM_ATOMIC_32_COMPARE_AND_STORE_NR, struct fam_atomic_args_32)
+#define FAM_ATOMIC_64_COMPARE_AND_STORE         _FAM_ATOMIC_WR(_FAM_ATOMIC_64_COMPARE_AND_STORE_NR, struct fam_atomic_args_64)
+#define FAM_ATOMIC_128_COMPARE_AND_STORE        _FAM_ATOMIC_WR(_FAM_ATOMIC_128_COMPARE_AND_STORE_NR, struct fam_atomic_args_128)
+
+/* p0 = *offset */
+#define FAM_ATOMIC_128_READ                     _FAM_ATOMIC_R(_FAM_ATOMIC_128_READ_NR, struct fam_atomic_args_128)
+
 /*
  * Register Fabric Attached Memory regions.
  *
- * NVM regions containing fam-atomics and/or fam-locks must be registered
+ * NVM regions containing fam atomics and/or fam locks must be registered
  * before any of the atomics or locks within the region can be used.
  *
  * @region_start: Address of the start of the NVM region.
@@ -28,89 +67,89 @@
  *
  * Return: 0 if the register function succeeds, else a negative value.
  */
-extern int
+int
 fam_atomic_register_region(void *region_start,
 			   size_t region_length,
 			   int fd,
 			   off_t offset);
 
-extern void
+void
 fam_atomic_unregister_region(void *region_start,
 			     size_t region_length);
 
 /*
  * Fabric Attached Memory Atomic un-aligned API.
  *
- * The following are the unpadded variant of the fam-atomics. Users of
- * the atomics below must manually keep the fam-atomic variables in their
- * own cachelines so that they do not share cachelines with regular data.
- * This is required to ensure the correctness of the atomic data.
+ * The following are the unpadded variant of the fam atomics. Users of
+ * these unpadded atomics must manually keep the fam atomic variables in
+ * their own cachelines so that they do not share cachelines with regular
+ * data. This is required to ensure the correctness of the atomic data.
  */
-extern int32_t
+int32_t
 fam_atomic_32_fetch_and_add_unpadded(int32_t *address,
 				     int32_t increment);
 
-extern int64_t
+int64_t
 fam_atomic_64_fetch_and_add_unpadded(int64_t *address,
 				     int64_t increment);
 
-extern int32_t
+int32_t
 fam_atomic_32_swap_unpadded(int32_t *address,
 			    int32_t value);
 
-extern int64_t
+int64_t
 fam_atomic_64_swap_unpadded(int64_t *address,
 			    int64_t value);
 
-extern void
+void
 fam_atomic_128_swap_unpadded(int64_t *address,
 			     int64_t value[2],
 			     int64_t result[2]);
 
-extern int32_t
+int32_t
 fam_atomic_32_compare_and_store_unpadded(int32_t *address,
 					 int32_t compare,
 					 int32_t store);
 
-extern int64_t
+int64_t
 fam_atomic_64_compare_and_store_unpadded(int64_t *address,
 					 int64_t compare,
 					 int64_t store);
 
-extern void
+void
 fam_atomic_128_compare_and_store_unpadded(int64_t *address,
 					  int64_t compare[2],
 					  int64_t store[2],
 					  int64_t result[2]);
 
-extern int32_t
+int32_t
 fam_atomic_32_read_unpadded(int32_t *address);
 
-extern int64_t
+int64_t
 fam_atomic_64_read_unpadded(int64_t *address);
 
-extern void
+void
 fam_atomic_128_read_unpadded(int64_t *address,
 			     int64_t result[2]);
 
-extern void
+void
 fam_atomic_32_write_unpadded(int32_t *address,
 			     int32_t value);
 
-extern void
+void
 fam_atomic_64_write_unpadded(int64_t *address,
 			     int64_t value);
 
-extern void
+void
 fam_atomic_128_write_unpadded(int64_t *address,
 			      int64_t value[2]);
 
 /*
- * fam-atomic data types
+ * fam atomic data types
  * ----------------------
- * struct fam_atomic_32 - 32 bit fam-atomic type
- * struct fam_atomic_64 - 64 bit fam-atomic type
- * struct fam_atomic_128 - 128 bit fam-atomic type
+ * struct fam_atomic_32 - 32 bit fam atomic type
+ * struct fam_atomic_64 - 64 bit fam atomic type
+ * struct fam_atomic_128 - 128 bit fam atomic type
  */
 struct fam_atomic_32 {
 	int32_t __v__ __attribute__((__aligned__(64)));
@@ -128,94 +167,142 @@ struct fam_atomic_128 {
  * Atomically adds "increment" to the atomic variable and returns the
  * previous value of the atomic variable.
  *
- * @address: Pointer to an fam-atomic variable.
+ * @address: Pointer to an fam atomic variable.
  * @increment: The value which will be added to the atomic.
  */
-extern int32_t
+static inline int32_t
 fam_atomic_32_fetch_and_add(struct fam_atomic_32 *address,
-			    int32_t increment);
+			    int32_t increment)
+{
+	return fam_atomic_32_fetch_and_add_unpadded(&address->__v__,
+						    increment);
+}
 
-extern int64_t
+static inline int64_t
 fam_atomic_64_fetch_and_add(struct fam_atomic_64 *address,
-			    int64_t increment);
+			    int64_t increment)
+{
+	return fam_atomic_64_fetch_and_add_unpadded(&address->__v__,
+						    increment);
+}
 
 /*
  * Atomically writes "value" to the atomic variable and returns
  * the previous value of the atomic variable.
  *
- * @address: Pointer to an fam-atomic variable.
+ * @address: Pointer to an fam atomic variable.
  * @value: The new value that will be written to the atomic.
  */
-extern int32_t
+static inline int32_t
 fam_atomic_32_swap(struct fam_atomic_32 *address,
-		   int32_t value);
+		   int32_t value)
+{
+	return fam_atomic_32_swap_unpadded(&address->__v__, value);
+}
 
-extern int64_t
+static inline int64_t
 fam_atomic_64_swap(struct fam_atomic_64 *address,
-		   int64_t value);
+		   int64_t value)
+{
+	return fam_atomic_64_swap_unpadded(&address->__v__, value);
+}
 
-extern void
+static inline void
 fam_atomic_128_swap(struct fam_atomic_128 *address,
 		    int64_t value[2],
-		    int64_t result[2]);
+		    int64_t result[2])
+{
+	fam_atomic_128_swap_unpadded(address->__v__, value, result);
+}
 
 /*
- * Atomically checks if the atomic variable is equal to "expected"
- * and sets the atomic to "desired" if true. Returns 1 if the operations
- * succeeded in modifying the atomic, else returns 0.
+ * Atomically checks if the atomic variable is equal to "compare"
+ * and sets the atomic to "store" if true. Returns the previous
+ * value of the atomic varible.
  *
- * @address: Pointer to an fam-atomic variable.
- * @expected: The value which the atomic is expected to equal.
- * @desired: The value the atomic will be set to if equal to "expected".
+ * @address: Pointer to an fam atomic variable.
+ * @compare The value which the atomic is expected to equal.
+ * @store: The value the atomic will be set to if equal to "compare".
  */
-extern int32_t
+static inline int32_t
 fam_atomic_32_compare_and_store(struct fam_atomic_32 *address,
 				int32_t compare,
-				int32_t store);
+				int32_t store)
+{
+	return fam_atomic_32_compare_and_store_unpadded(&address->__v__,
+							compare, store);
+}
 
-extern int64_t
+static inline int64_t
 fam_atomic_64_compare_and_store(struct fam_atomic_64 *address,
 				int64_t compare,
-				int64_t store);
+				int64_t store)
+{
+	return fam_atomic_64_compare_and_store_unpadded(&address->__v__,
+							compare, store);
+}
 
-extern void
+static inline void
 fam_atomic_128_compare_and_store(struct fam_atomic_128 *address,
 				 int64_t compare[2],
 				 int64_t store[2],
-				 int64_t result[2]);
+				 int64_t result[2])
+{
+	fam_atomic_128_compare_and_store_unpadded(address->__v__, compare,
+						  store, result);
+}
 
 /*
  * Returns the value of the atomic variable.
  *
- * @address: Pointer to an fam-atomic variable.
+ * @address: Pointer to an fam atomic variable.
  */
-extern int32_t
-fam_atomic_32_read(struct fam_atomic_32 *address);
+static inline int32_t
+fam_atomic_32_read(struct fam_atomic_32 *address)
+{
+	return fam_atomic_32_read_unpadded(&address->__v__);
+}
 
-extern int64_t
-fam_atomic_64_read(struct fam_atomic_64 *address);
+static inline int64_t
+fam_atomic_64_read(struct fam_atomic_64 *address)
+{
+	return fam_atomic_64_read_unpadded(&address->__v__);
+}
 
-extern void
+static inline void
 fam_atomic_128_read(struct fam_atomic_128 *address,
-		    int64_t result[2]);
+		    int64_t result[2])
+{
+	fam_atomic_128_read_unpadded(address->__v__, result);
+}
 
 /*
  * Writes "value" to the atomic variable.
  *
- * @address: Pointer to an fam-atomic variable.
+ * @address: Pointer to an fam atomic variable.
  * @value: The value that will be written to the atomic.
  */
-extern void
+static inline void
 fam_atomic_32_write(struct fam_atomic_32 *address,
-		    int32_t value);
+		    int32_t value)
+{
+	fam_atomic_32_write_unpadded(&address->__v__, value);
+}
 
-extern void
+static inline void
 fam_atomic_64_write(struct fam_atomic_64 *address,
-		    int64_t value);
+		    int64_t value)
+{
+	fam_atomic_64_write_unpadded(&address->__v__, value);
+}
 
-extern void
+static inline void
 fam_atomic_128_write(struct fam_atomic_128 *address,
-		     int64_t value[2]);
+		     int64_t value[2])
+{
+	fam_atomic_128_write_unpadded(address->__v__, value);
+}
+
 
 /* Spinlocks */
 typedef int32_t	__fam_ticket_t;
@@ -249,14 +336,52 @@ struct fam_spinlock_unpadded {
 
 #define FAM_SPINLOCK_UNPADDED_INITIALIZER       { .head_tail = 0 }
 
-extern void
-fam_spin_lock_unpadded(struct fam_spinlock_unpadded *lock);
+static inline void
+fam_spin_lock_unpadded(struct fam_spinlock_unpadded *lock)
+{
+	struct fam_spinlock_unpadded inc = {
+		.tickets = {
+			.head = 0,
+			.tail = 1
+		}
+	};
 
-extern bool
-fam_spin_trylock_unpadded(struct fam_spinlock_unpadded *lock);
+	/* Fetch the current values and bump the tail by one */
+	inc.head_tail = fam_atomic_64_fetch_and_add_unpadded(&lock->head_tail, inc.head_tail);
 
-extern void
-fam_spin_unlock_unpadded(struct fam_spinlock_unpadded *lock);
+	if (inc.tickets.head != inc.tickets.tail) {
+		for (;;) {
+			inc.tickets.head = fam_atomic_32_fetch_and_add_unpadded(&lock->tickets.head, 0);
+			if (inc.tickets.head == inc.tickets.tail)
+				break;
+		}
+	}
+	__sync_synchronize();
+}
+
+static inline bool
+fam_spin_trylock_unpadded(struct fam_spinlock_unpadded *lock)
+{
+	struct fam_spinlock_unpadded old, new;
+	bool ret;
+
+	old.head_tail = fam_atomic_64_fetch_and_add_unpadded(&lock->head_tail, (int64_t) 0);
+	if (old.tickets.head != old.tickets.tail)
+		return 0;
+
+	new.tickets.head = old.tickets.head;
+	new.tickets.tail = old.tickets.tail + 1;
+	ret = fam_atomic_64_compare_and_store_unpadded(&lock->head_tail, old.head_tail, new.head_tail) == old.head_tail;
+	__sync_synchronize();
+	return ret;
+}
+
+static inline void
+fam_spin_unlock_unpadded(struct fam_spinlock_unpadded *lock)
+{
+	(void) fam_atomic_32_fetch_and_add_unpadded(&lock->tickets.head, 1);
+	__sync_synchronize();
+}
 
 struct fam_spinlock {
 	struct fam_spinlock_unpadded __v__ __attribute((__aligned__(64)));
@@ -264,13 +389,22 @@ struct fam_spinlock {
 
 #define FAM_SPINLOCK_INITIALIZER	{ .__v__ = { .head_tail = 0 } }
 
-extern void
-fam_spin_lock(struct fam_spinlock *lock);
+static inline void
+fam_spin_lock(struct fam_spinlock *lock)
+{
+	fam_spin_lock_unpadded(&lock->__v__);
+}
 
-extern bool
-fam_spin_trylock(struct fam_spinlock *lock);
+static inline bool
+fam_spin_trylock(struct fam_spinlock *lock)
+{
+	return fam_spin_trylock_unpadded(&lock->__v__);
+}
 
-extern void
-fam_spin_unlock(struct fam_spinlock *lock);
+static inline void
+fam_spin_unlock(struct fam_spinlock *lock)
+{
+	return fam_spin_unlock_unpadded(&lock->__v__);
+}
 
 #endif
