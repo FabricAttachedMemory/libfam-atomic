5c489689bded6a3d4638182cdd7018205f9a12b6
Jason Low <jason.low2@hp.com>
2015-10-12 12:22:20 -0700

    Add GPLv3+ licensing to source files
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- fam_atomic.h
+++ fam_atomic.h
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2015 Jason Low <jason.low2@hpe.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #ifndef _FAM_ATOMIC_H_
 #define _FAM_ATOMIC_H_
 
@@ -369,4 +383,4 @@ fam_spin_unlock(struct fam_spinlock *lock)
 	fam_spin_unlock_unpadded(&lock->__v__);
 }
 
-#endif
+#endif /* _FAM_ATOMIC_H_ */
